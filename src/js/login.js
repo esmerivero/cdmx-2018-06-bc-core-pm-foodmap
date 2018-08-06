fe8a73281d3d69bead3c9fcb4bf558d529e0ac0e
lameme <esme.riveroh@gmail.com>
2018-08-06 12:42:31 -0500

    Obteniendo la información de la base de datos

---
+++ src/js/login.js
@@ -0,0 +1,6 @@
+window.onload = () =>{
+    let buttonLogin = document.getElementById('button-login');
+    buttonLogin.addEventListener('click', function(){
+        window.location.assign('home.html');
+    });
+}
