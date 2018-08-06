8d69161b0bcaee8fcf3b8701a1a750244c05f3eb
lameme <esme.riveroh@gmail.com>
2018-08-06 18:10:40 -0500

    Agregando funcionalidad de busqueda de los botones

--- src/js/foodmap.js
+++ src/js/foodmap.js
@@ -1,26 +1,77 @@
 let config = {
-    apiKey: "AIzaSyDmI0IERvjSq0334ndUnRMb09SyKuE4mMI",
-    authDomain: "taller-firebase-4e2b1.firebaseapp.com",
-    databaseURL: "https://taller-firebase-4e2b1.firebaseio.com",
-    projectId: "taller-firebase-4e2b1",
-    storageBucket: "taller-firebase-4e2b1.appspot.com",
-    messagingSenderId: "521580200060"
+    apiKey: 'AIzaSyDmI0IERvjSq0334ndUnRMb09SyKuE4mMI',
+    authDomain: 'taller-firebase-4e2b1.firebaseapp.com',
+    databaseURL: 'https://taller-firebase-4e2b1.firebaseio.com',
+    projectId: 'taller-firebase-4e2b1',
+    storageBucket: 'taller-firebase-4e2b1.appspot.com',
+    messagingSenderId: '521580200060'
 };
 
 firebase.initializeApp(config);
 let db = firebase.firestore();
+let restaurantsRef = db.collection('restaurants');
+const btnComidaMexicana = document.getElementById('tipo-mexicana');
+const btnComidaChina = document.getElementById('tipo-china');
+const btnComidaItaliana = document.getElementById('tipo-italiana');
+const btnSnacks = document.getElementById('snacks');
+const btnMejoresCalificados = document.getElementById('raking');
+const btnMejoresPrecios = document.getElementById('cost');
+const cardRestaurant = document.getElementById('card');
 
-let restaurantsRef = db.collection("restaurants");
+let idSearch = '';
+let equalSearch = "==";
+let idComparation;
 
-restaurantsRef.where("name", "==", 'taquito feliz')
+btnComidaMexicana.addEventListener('click', function(){
+    cardRestaurant.innerHTML = '';
+    clickBoton('type',equalSearch,'comida mexicana');
+});
+
+btnComidaChina.addEventListener('click', function(){
+    cardRestaurant.innerHTML = '';
+    clickBoton('type',equalSearch,'comida china');
+});
+
+btnComidaItaliana.addEventListener('click', function(){
+    cardRestaurant.innerHTML = '';
+    clickBoton('type',equalSearch,'comida italiana');
+});
+
+btnSnacks.addEventListener('click', function(){
+    cardRestaurant.innerHTML = '';
+    clickBoton('type',equalSearch,'snacks')
+});
+
+btnMejoresCalificados.addEventListener('click', function(){
+    cardRestaurant.innerHTML = '';
+    clickBoton('rating', '>=' , 4)
+});
+
+btnMejoresPrecios.addEventListener('click', function(){
+    cardRestaurant.innerHTML = '';
+    clickBoton('cost', '<=' , 150)
+});
+
+function clickBoton (idSearch, equalSearch, idComparation){
+    restaurantsRef.where(idSearch, equalSearch, idComparation)
     .get()
     .then(function(querySnapshot) {
         querySnapshot.forEach(function(doc) {
-            // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
-            return (doc.data());
+            let nombre = doc.data().name;
+            let direccion = doc.data().address;
+            let costo = doc.data().cost;
+            let tipoComida = doc.data().type;
+            let puntuacion = doc.data().rating;
+
+            cardRestaurant.innerHTML += `Nombre: ${nombre} <br>
+                                        Dirección: ${direccion} <br>
+                                        Costo aprox. por persona: ${costo} <br>
+                                        Tipo de comida: ${tipoComida} <br>
+                                        Puntuación: ${puntuacion} <hr>`;
+            console.log(doc.id, doc.data());
         });
     })
     .catch(function(error) {
-        console.log("Error getting documents: ", error);
+        console.log('Error al obtener la información: ', error);
 });
+}
